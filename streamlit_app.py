8bdb8eab8378b7f7c3158a3379862b1f39b184d1
brettchunghyeon <chunghyeon.kim@sk.com>
2024-10-16 07:38:23 +0000

    내가 만든 앱

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("내가 만든 앱")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "wow 신호 (https://docs.streamlit.io/)."
 )
+
